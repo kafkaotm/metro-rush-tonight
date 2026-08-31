d39f415b739fc19d284249abcbb40453a679e302
kafkaotm <kafkaotm@gmail.com>
2026-08-31 14:22:05 +0800

    feat: implement LineSelector

---
+++ src/components/LineSelector.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { Line } from '../logic/types'
+
+defineProps<{
+  lines: Line[]
+}>()
+
+const emit = defineEmits<{
+  select: [lineId: string]
+}>()
+</script>
+
+<template>
+  <ul class="line-selector">
+    <li
+      v-for="line in lines"
+      :key="line.LineID"
+    >
+      <button
+        type="button"
+        data-testid="line-option"
+        :style="{ backgroundColor: line.LineColor }"
+        @click="emit('select', line.LineID)"
+      >
+        {{ line.LineName.Zh_tw }}
+      </button>
+    </li>
+  </ul>
+</template>
