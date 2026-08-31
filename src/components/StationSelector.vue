16c03e4c403a246b02bc5d35719dc693760806ef
kafkaotm <kafkaotm@gmail.com>
2026-08-31 16:24:56 +0800

    feat: implement StationSelector
    Vertical list of StationButton, one per station. Takes lineColor as
    a prop (alongside stations) since the platform dot needs it and
    Station itself carries no color.

---
+++ src/components/StationSelector.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import StationButton from './StationButton.vue'
+import type { Station } from '../logic/types'
+
+defineProps<{
+  stations: Station[]
+  lineColor: string
+}>()
+
+const emit = defineEmits<{
+  select: [stationId: string]
+}>()
+</script>
+
+<template>
+  <ul class="m-0 flex list-none flex-col gap-[8px] p-0">
+    <li
+      v-for="station in stations"
+      :key="station.StationID"
+    >
+      <StationButton
+        :station="station"
+        :line-color="lineColor"
+        data-testid="station-option"
+        @click="emit('select', station.StationID)"
+      />
+    </li>
+  </ul>
+</template>
